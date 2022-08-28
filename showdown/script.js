27c2d8ae4653ed2f904e44c779b40eb37546d47f
mdecycu At cycu dot org <101738344+mdecycu@users.noreply.github.com>
2022-08-28 21:00:06 +0800

    Update script.js

--- showdown/script.js
+++ showdown/script.js
@@ -1,5 +1,42 @@
 "use strict";
 
+// 👇️ if using ES6 Imports uncomment line below
+// import {readFileSync, promises as fsPromises} from 'fs';
+const {readFileSync, promises: fsPromises} = require('fs');
+
+// ✅ read file SYNCHRONOUSLY
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+
+  const arr = contents.split(/\r?\n/);
+
+  console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
+
+  return arr;
+}
+
+//syncReadFile('./example.txt');
+
+// --------------------------------------------------------------
+
+// ✅ read file ASYNCHRONOUSLY
+async function asyncReadFile(filename) {
+  try {
+    const contents = await fsPromises.readFile(filename, 'utf-8');
+
+    const arr = contents.split(/\r?\n/);
+
+    console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
+
+    return arr;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//asyncReadFile('./example.txt');
+
+
 var worker = new Worker("showdown/worker.js");
 worker.onmessage = function(message) {
 	document.body.innerHTML = message.data;
